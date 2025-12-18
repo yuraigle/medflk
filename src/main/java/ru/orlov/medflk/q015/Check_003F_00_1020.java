9b943c81912ace6c5dc2e4688f6365c7105d92a3
yuraigle <yuraigle@gmail.com>
2025-12-18 08:00:20 +0800

    ...

--- src/main/java/ru/orlov/medflk/q015/Check_003F_00_1020.java
+++ src/main/java/ru/orlov/medflk/q015/Check_003F_00_1020.java
@@ -2,7 +2,6 @@ package ru.orlov.medflk.q015;
 
 import org.springframework.stereotype.Component;
 import ru.orlov.medflk.jaxb.FlkErr;
-import ru.orlov.medflk.jaxb.OnkSl;
 import ru.orlov.medflk.jaxb.PersList;
 import ru.orlov.medflk.jaxb.ZlList;
 
@@ -18,14 +17,11 @@ public class Check_003F_00_1020 extends AbstractCheck {
 
     @Override
     public List<FlkErr> check(ZlList zlList, PersList persList) {
-        return iterateOverSl(zlList, persList, (a, zap, sl) -> {
-            OnkSl onkSl = sl.getOnkSl();
-            if (onkSl != null) {
-                Integer ds1T = onkSl.getDs1T();
-                Integer mtstz = onkSl.getMtstz();
-                if (ds1T != null && !List.of(1, 2).contains(ds1T) && mtstz != null) {
-                    return List.of(new FlkErr(zap, sl, null, onkSl.getMtstz()));
-                }
+        return iterateOverOnkSl(zlList, persList, (a, zap, sl, onkSl) -> {
+            Integer ds1T = onkSl.getDs1T();
+            Integer mtstz = onkSl.getMtstz();
+            if (ds1T != null && !List.of(1, 2).contains(ds1T) && mtstz != null) {
+                return List.of(new FlkErr(zap, sl, null, mtstz));
             }
 
             return List.of();

41acd88c1f532ba6878e5c078bb622d99af8920f
yuraigle <yuraigle@gmail.com>
2025-12-21 23:12:47 +0800

    ...

--- src/main/java/ru/orlov/medflk/controller/HomeController.java
+++ src/main/java/ru/orlov/medflk/controller/HomeController.java
@@ -9,22 +9,29 @@ import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import ru.orlov.medflk.MedflkFxApplication;
 import ru.orlov.medflk.domain.ValidationResult;
+import ru.orlov.medflk.domain.nsi.AbstractNsiService;
+import ru.orlov.medflk.service.FileValidatorService;
+import ru.orlov.medflk.service.NsiDownloaderService;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Log4j2
 @Controller
 @RequiredArgsConstructor
 public class HomeController implements Initializable {
 
-    private final TaskInitNsi taskInitNsi;
-    private final TaskDownloadNsi taskDownloadNsi;
-    private final TaskValidateFile taskValidateFile;
+    private final ApplicationContext ctx;
+    private final NsiDownloaderService nsiDownloaderService;
+    private final FileValidatorService validator;
 
     @FXML
     private TextArea vLog;
@@ -40,12 +47,98 @@ public class HomeController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        runInBackground(taskInitNsi);
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                List<String> nsiServices = Arrays.stream(ctx.getBeanDefinitionNames())
+                        .filter(name -> name.matches("^.*[a-zA-Z][0-9]{3}Service$"))
+                        .toList();
+                int ttl = nsiServices.size();
+                AtomicInteger cntReady = new AtomicInteger(0);
+
+                nsiServices.forEach(name -> {
+                    updateMessage("Инициализируем справочники " + cntReady.incrementAndGet() + "/" + ttl);
+                    Object bean = ctx.getBean(name);
+                    if (bean instanceof AbstractNsiService) {
+                        ((AbstractNsiService) bean).initPacket();
+                    }
+                });
+                return null;
+            }
+
+            @Override
+            protected void scheduled() {
+                statusLine.textProperty().bind(messageProperty());
+                btnSelectFile.setDisable(true);
+            }
+
+            @Override
+            protected void succeeded() {
+                statusLine.textProperty().unbind();
+                statusLine.setText("");
+                btnSelectFile.setDisable(false);
+            }
+
+            @Override
+            protected void failed() {
+                statusLine.textProperty().unbind();
+                statusLine.setText("Ошибка чтения справочников");
+            }
+        };
+
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     @FXML
     void downloadNsi() {
-        runInBackground(taskDownloadNsi);
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                List<String> packages1 = nsiDownloaderService.getFfomsPackages();
+                List<String> packages2 = nsiDownloaderService.getRmzPackages().keySet().stream().toList();
+                int ttl = packages1.size() + packages2.size();
+                AtomicInteger cntReady = new AtomicInteger(0);
+
+                packages1.forEach(p -> {
+                    updateMessage("Скачиваем справочники " + cntReady.incrementAndGet() + "/" + ttl);
+                    nsiDownloaderService.updateFfoms(p);
+                });
+
+                packages2.forEach(p -> {
+                    updateMessage("Скачиваем справочники " + cntReady.incrementAndGet() + "/" + ttl);
+                    nsiDownloaderService.updateRmz(p);
+                });
+                return null;
+            }
+
+            @Override
+            protected void scheduled() {
+                statusLine.textProperty().bind(messageProperty());
+                btnUpdateNsi.setDisable(true);
+            }
+
+            @Override
+            protected void succeeded() {
+                statusLine.textProperty().unbind();
+                statusLine.setText("");
+                btnUpdateNsi.setDisable(false);
+                btnSelectFile.setDisable(false);
+            }
+
+            @Override
+            protected void failed() {
+                statusLine.textProperty().unbind();
+                statusLine.setText("Ошибка при попытке скачать справочник");
+                btnUpdateNsi.setDisable(false);
+                btnSelectFile.setDisable(false);
+            }
+        };
+
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     @FXML
@@ -57,43 +150,40 @@ public class HomeController implements Initializable {
 
         File file = fileChooser.showOpenDialog(MedflkFxApplication.primaryStage);
         if (file != null) {
-            vLog.setText("");
-            btnSelectFile.setDisable(true);
-            statusLine.setText("Проверяем файл " + file.getName());
+            Task<ValidationResult> task = new Task<>() {
+                @Override
+                protected ValidationResult call() {
+                    updateMessage("Проверяем файл " + file.getName());
+                    ValidationResult result = validator.validate(file);
+                    vLog.setText(result.toString());
+                    return null;
+                }
 
-            taskValidateFile.setFile(file);
-            taskValidateFile.setOnSucceeded(ev -> {
-                ValidationResult res = (ValidationResult) ev.getSource().getValue();
-                vLog.setText(res.toString());
-                btnSelectFile.setDisable(false);
-                statusLine.setText("");
-                taskValidateFile.setFile(null);
-                taskValidateFile.setOnSucceeded(null);
-            });
+                @Override
+                protected void scheduled() {
+                    statusLine.textProperty().bind(messageProperty());
+                    vLog.setText("");
+                    btnSelectFile.setDisable(true);
+                }
 
-            Thread thread = new Thread(taskValidateFile);
+                @Override
+                protected void succeeded() {
+                    statusLine.textProperty().unbind();
+                    statusLine.setText("");
+                    btnSelectFile.setDisable(false);
+                }
+
+                @Override
+                protected void failed() {
+                    statusLine.textProperty().unbind();
+                    statusLine.setText("Ошибка при проверке файла");
+                    btnSelectFile.setDisable(false);
+                }
+            };
+
+            Thread thread = new Thread(task);
             thread.setDaemon(true);
             thread.start();
         }
     }
-
-    private void runInBackground(Task<Void> task) {
-        btnUpdateNsi.setDisable(true);
-        btnSelectFile.setDisable(true);
-
-        Thread thread = new Thread(task);
-        thread.setDaemon(true);
-        thread.start();
-
-        task.setOnScheduled(ev ->
-                statusLine.textProperty().bind(task.messageProperty()));
-        task.setOnFailed(ev ->
-                statusLine.textProperty().unbind());
-        task.setOnSucceeded(ev -> {
-            statusLine.textProperty().unbind();
-            btnUpdateNsi.setDisable(false);
-            btnSelectFile.setDisable(false);
-        });
-    }
-
 }

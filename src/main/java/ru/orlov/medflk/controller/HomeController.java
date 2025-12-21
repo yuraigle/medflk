b42473c44fe246f87743e2162ad366dfbcf0623e
yuraigle <yuraigle@gmail.com>
2025-12-21 20:34:18 +0800

    ...

--- src/main/java/ru/orlov/medflk/controller/HomeController.java
+++ src/main/java/ru/orlov/medflk/controller/HomeController.java
@@ -7,14 +7,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import ru.orlov.medflk.MedflkFxApplication;
 import ru.orlov.medflk.domain.ValidationResult;
-import ru.orlov.medflk.service.FileValidatorService;
-import ru.orlov.medflk.service.NsiDownloaderService;
 
 import java.io.File;
 import java.net.URL;
@@ -22,11 +19,12 @@ import java.util.ResourceBundle;
 
 @Log4j2
 @Controller
+@RequiredArgsConstructor
 public class HomeController implements Initializable {
 
-    private final ApplicationContext ctx;
-    private final FileValidatorService validator;
-    private final NsiDownloaderService nsiDownloaderService;
+    private final TaskInitNsi taskInitNsi;
+    private final TaskDownloadNsi taskDownloadNsi;
+    private final TaskValidateFile taskValidateFile;
 
     @FXML
     private TextArea vLog;
@@ -40,27 +38,14 @@ public class HomeController implements Initializable {
     @FXML
     private Button btnUpdateNsi;
 
-    @Lazy
-    public HomeController(
-            ApplicationContext ctx,
-            FileValidatorService validator,
-            NsiDownloaderService nsiDownloaderService
-    ) {
-        this.ctx = ctx;
-        this.validator = validator;
-        this.nsiDownloaderService = nsiDownloaderService;
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        TaskInitNsi task = new TaskInitNsi(ctx);
-        runInBackground(task);
+        runInBackground(taskInitNsi);
     }
 
     @FXML
     void downloadNsi() {
-        TaskDownloadNsi task = new TaskDownloadNsi(nsiDownloaderService);
-        runInBackground(task);
+        runInBackground(taskDownloadNsi);
     }
 
     @FXML
@@ -73,12 +58,24 @@ public class HomeController implements Initializable {
         File file = fileChooser.showOpenDialog(MedflkFxApplication.primaryStage);
         if (file != null) {
             vLog.setText("");
-            log.info("Selected file: {}", file.getAbsolutePath());
-            ValidationResult procLog = validator.validate(file);
-            vLog.setText(procLog.toString());
+            btnSelectFile.setDisable(true);
+            statusLine.setText("Проверяем файл " + file.getName());
+
+            taskValidateFile.setFile(file);
+            taskValidateFile.setOnSucceeded(ev -> {
+                ValidationResult res = (ValidationResult) ev.getSource().getValue();
+                vLog.setText(res.toString());
+                btnSelectFile.setDisable(false);
+                statusLine.setText("");
+                taskValidateFile.setFile(null);
+                taskValidateFile.setOnSucceeded(null);
+            });
+
+            Thread thread = new Thread(taskValidateFile);
+            thread.setDaemon(true);
+            thread.start();
         }
     }
-
 
     private void runInBackground(Task<Void> task) {
         btnUpdateNsi.setDisable(true);
